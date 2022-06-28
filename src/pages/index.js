0f84a7ec8a58ceec150e8302c7267cf6820e0f8f
Anthony <admin@amazastrophic.com>
2022-06-28 12:57:58 -0400

    hh
    hh

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import React, { useState } from "react";
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-
+import styles from './styles.module.css';
 
 
 const features = [
